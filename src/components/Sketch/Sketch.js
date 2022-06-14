925ee0331924ab2e025d3a4f48570e07540a970b
Valerie <valdaraa@protonmail.com>
2022-06-14 18:34:17 -0400

    MVP zorb design #2, currently not outputting anything

--- src/components/Sketch/Sketch.js
+++ src/components/Sketch/Sketch.js
@@ -3,9 +3,30 @@ import BaseSketch from 'react-p5'
 
 const windowWidth = 500
 const windowHeight = 500
+
+let j = 0; // nest loop var, continous motion in loop
+let zorbRadius = 2; //size of balls
+let speedWarp = 10; //if var "N" goes up this number has to go down
+let density = 5; //the higher the more compact; values: {0.25,0.5,1,2,3,4,-.25,-.5,-1,-2,-3,-4}
+let spacingY = 5; //the higher the more chaotic; values: {1,2,3,4,5,-1,-2,-3,-4,-5}
+let spacingX = -3; //the higher the more chaotic; values: {1,2,3,4,5,6,-1,-2,-3,-4,-5,-6}
+//let noise = 0; 
+let N = 0;
+let K = 0;
+
 const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 	const [t, setT] = useState(0)
 	const [zorbArray, setZorbArray] = useState([])
+
+	const preload = p5 => {
+		const myImages = []
+
+		for (let i = 0; i < zorbs.length; i++) {
+			myImages.push(p5.loadImage(zorbs[i]))
+		}
+
+		setZorbArray(myImages)
+	}
 
 	const setup = (p5, canvasParentRef) => {
 		// use parent to render the canvas in this ref
@@ -14,43 +35,55 @@ const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 	}
 
 	const draw = p5 => {
-		const steps = 0.002 //speed
-		setT((t += steps))
-		const fluid = 0.009
-		const zorbRadius = 24 //size of balls
-		const w = 500
-		const h = 500
-		const mvx = 50
-		const mvy = 10
-		p5.background(0, w)
-		const NUMBER_OF_ROWS = Math.floor(Math.sqrt((5 * zoraEvents.length) / 4))
-		const NUMBER_OF_COLUMNS = Math.ceil(zoraEvents.length / NUMBER_OF_ROWS)
 
-		for (let x = 0; x < NUMBER_OF_COLUMNS; x += 1)
-			for (let y = 0; y < NUMBER_OF_ROWS; y += 1) {
-				const n = _ => {
-					return p5.TAU * (t + p5.sin(p5.TAU * t - p5.dist(25 * x, 20 * y, w / 2, h / 2) * fluid))
+		//-------------v------old design------v--------------------------
+
+		/*
+		for (let i = 0; i < N; x++)
+			for (j = 0; j < N; j++) {
+				
+				const xCoordinate = i % N * K - 1;
+				const yCoordinate = i / N * K - 1;
+				const s = _ => {
+					return Math.sin(j * p5.TAU * / speedWarp + 8 * Math.sqrt(density + Math.sin(y * spacingY) ** 2 + (y + sin(x * spacingX)) ** 2));
 				}
-				const xCoordinate = (windowWidth / NUMBER_OF_COLUMNS) * x + mvx * p5.sin(n())
-				const yCoordinate = (windowHeight / NUMBER_OF_ROWS) * y + mvy * p5.cos(n())
+				const d = _ => {
+					return p5.dist(0, 0, 0, xCoordinate, yCoordinate, s / 8 + 0.5)
+				}
 
-				let nz = 100
-				nz = p5.noise(15 * x * fluid, 20 * y * fluid)
-				const zorbIndex = (x + 1) * (y + 1) - 1
+
 				if (zorbIndex < zoraEvents.length) {
-					p5.image(zorbArray[zorbIndex], xCoordinate, yCoordinate, zorbRadius, zorbRadius)
+					p5.image(zorbArray[zorbIndex], xCoordinate, yCoordinate, zorbRadius/d, zorbRadius/d)
 				}
 			}
+		-----------------------------------------------------------------
+		*/
+
+		p5.translate(125,125); //centering zorb, hard coded 
+		p5.background(0); //black
+		j++;  //motion
+		N = 75; // odd numbers, number of points on sphere, doesn't change after set, it's in proportion to window size and aesthetics
+		K = 2 / N; 
+		p5.colorMode(HSB, 1);  
+		
+		for (i = 0; i < (N * N); i++){ //loops to plot points in the the sphere width
+			zorbMotion();
+		} 
 	}
 
-	const preload = p5 => {
-		const myImages = []
-
-		for (let i = 0; i < zorbs.length; i++) {
-			myImages.push(p5.loadImage(zorbs[i]))
+	const zorbMotion = p5 => {
+		const x = i % N * K - 1;
+		const y = i / N * K - 1;
+		const s = _ => {
+			return Math.sin(j * p5.TAU / speedWarp + 8 * Math.sqrt(density + Math.sin(y * spacingY) ** 2 + (y + sin(x * spacingX)) ** 2));
 		}
-		setZorbArray(myImages)
+		const d = _ => {
+			return p5.dist(0, 0, 0, x, y, s / 8 + 0.5)
+		}
+	
+		p5.circle(zorbArray[zorbIndex],x / d * N + N, y / d * N + N, zorbRadius/d, zorbRadius/d);
 	}
+
 	return <BaseSketch setup={setup} draw={draw} preload={preload} />
 }
 
