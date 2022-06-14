14d9d886da670a53e00bb995dccee6dc3b697cca
Valerie <valdaraa@protonmail.com>
2022-06-14 18:40:38 -0400

    missing "Math." delineation

--- src/components/Sketch/Sketch.js
+++ src/components/Sketch/Sketch.js
@@ -36,7 +36,7 @@ const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 
 	const draw = p5 => {
 
-		//-------------v------old design------v--------------------------
+		//-------------v------trying to rework for loop into first code design------v--------------------------
 
 		/*
 		for (let i = 0; i < N; x++)
@@ -75,7 +75,7 @@ const Sketch = ({ zorbs, zoraEvents = { length: 500 } }) => {
 		const x = i % N * K - 1;
 		const y = i / N * K - 1;
 		const s = _ => {
-			return Math.sin(j * p5.TAU / speedWarp + 8 * Math.sqrt(density + Math.sin(y * spacingY) ** 2 + (y + sin(x * spacingX)) ** 2));
+			return Math.sin(j * p5.TAU / speedWarp + 8 * Math.sqrt(density + Math.sin(y * spacingY) ** 2 + (y + Math.sin(x * spacingX)) ** 2));
 		}
 		const d = _ => {
 			return p5.dist(0, 0, 0, x, y, s / 8 + 0.5)
